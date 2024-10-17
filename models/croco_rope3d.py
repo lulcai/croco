6b85db3a42324c25f0581eadc38f1c539cc273c9
llcai <1156264591@qq.com>
2024-10-17 15:45:34 +0800

    暂存block

--- models/croco_rope3d.py
+++ models/croco_rope3d.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn 
-from models.blocks import Attention, CrossAttention, Block, DecoderBlock, PatchEmbed
+from models.blocks import Attention, CrossAttention, Block, DecoderBlock, PatchEmbed, DropPath, Mlp
 from pdb import set_trace as bb
 torch.backends.cuda.matmul.allow_tf32 = True # for gpu >= Ampere and pytorch >= 1.12
 from functools import partial
@@ -72,7 +72,23 @@ class DecoderBlock_shared(DecoderBlock):
         x = x + self.drop_path(self.mlp(self.norm3(x)))
         return x, y
     
-     
+class DecoderBlock_noca(nn.Module):
+
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, rope=None):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = Attention(dim, rope=rope, num_heads=num_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop)
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+
+    def forward(self, x, xpos):
+        x = x + self.drop_path(self.attn(self.norm1(x), xpos))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x     
 
 class CroCoNet_addrope3d(nn.Module):
 
